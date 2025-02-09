1fd5ca7db3e49d09f2f50093f3bf07c0fb9dea73
Nagacharan <nagacharan4286@gmail.com>
2025-02-09 22:12:35 +0530

    deployment file changes

--- Terraform-Code/EKS-Cluster-Creation/eks.tf
+++ Terraform-Code/EKS-Cluster-Creation/eks.tf
@@ -1,31 +1,39 @@
-# DEFINE DEFAULT VARIABLES HERE
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.1"
 
-variable "instance_type" {
-  description = "Instance Type"
-  type        = string
-}
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
 
-variable "ami" {
-  description = "AMI ID"
-  type        = string
-}
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
-variable "key_name" {
-  description = "Key Pair"
-  type        = string
-}
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
 
-variable "volume_size" {
-  description = "Volume size"
-  type        = string
-}
+  eks_managed_node_groups = {
+    panda-node = {
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
 
-variable "region_name" {
-  description = "AWS Region"
-  type        = string
-}
+      instance_types = ["t2.medium"]
+      capacity_type  = "SPOT"
 
-variable "server_name" {
-  description = "EC2 Server Name"
-  type        = string
+      tags = {
+        ExtraTag = "Panda_Node"
+      }
+    }
+  }
+
+  tags = local.tags
 }
